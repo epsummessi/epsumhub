bfba86508e3c611cd66108acaa97ef6a894fa8d0
bcess <bcess@ulaval.ca>
2025-03-25 22:00:48 -0400

    final commit

--- src/firebase.js
+++ src/firebase.js
@@ -6,11 +6,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "next-blog-bac5b.firebaseapp.com",
-  projectId: "next-blog-bac5b",
-  storageBucket: "next-blog-bac5b.firebasestorage.app",
-  messagingSenderId: "282459795765",
-  appId: "1:282459795765:web:9392a74d8014b012c2dc7c"
+  authDomain: "epsummessihub-af7ae.firebaseapp.com",
+  projectId: "epsummessihub-af7ae",
+  storageBucket: "epsummessihub-af7ae.firebasestorage.app",
+  messagingSenderId: "385623348832",
+  appId: "1:385623348832:web:650b38cc43e07ef1701505"
 };
 
 // Initialize Firebase
